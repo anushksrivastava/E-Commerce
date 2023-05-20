789b475dde9c55a2a7f3a625f7d4708ce7880ab5
anushksrivastava <72188214+anushksrivastava@users.noreply.github.com>
2023-05-20 22:47:02 +0530

    Update Customer.java

--- e-commerce/src/main/java/com/example/ecommerce/model/Customer.java
+++ e-commerce/src/main/java/com/example/ecommerce/model/Customer.java
@@ -41,4 +41,76 @@ public class Customer {
 
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
     List<Ordered> orderList = new ArrayList<>();
+    
+     public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getMobNo() {
+        return mobNo;
+    }
+
+    public void setMobNo(String mobNo) {
+        this.mobNo = mobNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public List<Ordered> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(List<Ordered> orderList) {
+        this.orderList = orderList;
+    }
 }
